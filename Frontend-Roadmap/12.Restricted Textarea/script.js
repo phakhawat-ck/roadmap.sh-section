1e4af6f9f8a35d723719d4a180fad2f7ca9565a6
PIM <pim@example.com>
2025-04-19 03:23:30 +0700

    Restricted Textarea

---
+++ Frontend-Roadmap/12.Restricted Textarea/script.js
@@ -0,0 +1,25 @@
+
+const message = document.getElementById('message')
+const text = document.getElementById('limit')
+const maxChars = 250;
+
+
+const updateCount =() =>{
+    const countChar = message.value.length
+    // if countChar > maxChars  ?   T : maxChars  F : countChar
+    const displayCount = countChar > maxChars ? maxChars : countChar
+
+    text.innerText = `${displayCount} / ${maxChars}`
+    message.value = message.value.slice(0, maxChars)
+
+    
+    message.style.borderColor = countChar > maxChars ? 'red' : 'black'
+}
+
+updateCount()
+
+message.addEventListener('input', updateCount);
+
+
+
+
