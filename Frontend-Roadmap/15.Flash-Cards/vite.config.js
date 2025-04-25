dfaa685a8d54041c834118a0c34da19c6d42e1f2
PIM <pim@example.com>
2025-04-25 22:55:27 +0700

    fix

--- Frontend-Roadmap/15.Flash-Cards/vite.config.js
+++ Frontend-Roadmap/15.Flash-Cards/vite.config.js
@@ -4,4 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  base: "/roadmap.sh-section/Frontend-Roadmap/15.Flash-Cards",
 })
